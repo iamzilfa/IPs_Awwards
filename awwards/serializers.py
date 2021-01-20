318e04b6dc64432ceb356f53974b9f9c1142650e
Zilfa Cyamani <zilcyam@gmail.com>
2021-01-20 19:57:23 +0200

    modified awwards/serializers.py

--- awwards/serializers.py
+++ awwards/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Profile, Project
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('bio','contact','profile_picture',)
+
         
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
